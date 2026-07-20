6f12ab9e2b3ddb2141f88be647d1f94dad1ac902
Nova <magellandevoue3@gmail.com>
2026-07-21 00:21:13 +0100

    Add files

--- local-chain/scripts/send-test-deposit.js
+++ local-chain/scripts/send-test-deposit.js
@@ -6,8 +6,8 @@
 
 const hre = require('hardhat');
 
-const DEPOSIT_ADDRESS = '0xe1A18819AAC588b01e430E769D7042261054505C'; // depuis wallet.html
-const AMOUNT = '50'; // en mUSDT
+const DEPOSIT_ADDRESS = '0x47312D97AEf1FDe5adF9ecade791351313E3e6D3'; // depuis wallet.html
+const AMOUNT = '100000'; // en mUSDT
 const USDT_CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; // affichée par deploy.js
 
 async function main() {
